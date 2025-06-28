ef65a53297b0cbea094cefb49f610dd773db9fc6
Simon Llewellyn <llewells86@gmail.com>
2025-06-28 14:16:36 +0100

    added testing of openai api key

---
+++ learning/testing_openai_api.py
@@ -0,0 +1,18 @@
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    store=True,
+    messages=[{"role": "user", "content": "write a haiku about ai"}],
+)
+
+print(completion.choices[0].message)
